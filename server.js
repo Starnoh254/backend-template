7661bc1690c76b6e7c871d66078bbf2a546c81fb
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2025-06-11 17:18:17 +0300

    Implement authentication features with user registration and login, add error handling middleware, and integrate logging using Winston

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const app = require('./src/app');
 const http = require('http');
+const morgan = require('morgan');
+const logger = require('./src/utils/logger');
+const errorHandler = require('./src/middlewares/errorHandler');
 
 const PORT = process.env.PORT || 5000;
+
+app.use(morgan('combined', {
+    stream: {
+        write: (message) => logger.info(message.trim())
+    }
+}));
+
+app.use(errorHandler)
+
 
 const server = http.createServer(app);
 
