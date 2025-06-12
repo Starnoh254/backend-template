5568a6b5baf19ed22ef154c04c0235a8bbe728dd
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2025-06-12 14:44:04 +0300

    Refactor authentication routes and enhance login response structure; add tests for user registration and login

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-// const authRoutes = require('./routes/auth.routes');
+const routes = require('./routes/index')
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use('/api/auth', authRoutes);
+app.use('/api/v1/', routes);
 
 app.get('/', (req, res) => res.send('API is running!'));
 
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -20,7 +20,7 @@ class authController {
     const { email, password } = req.body;
     try {
       const result = await authService.loginUser(email, password);
-      res.status(200).json(result);
+      res.status(200).json({status : "success", result});
     } catch (err) {
       if (err.message === 'Invalid credentials') {
         return res.status(400).json({ message: err.message });
